e0b09ca722d0119488293fb69933d0aaa58f0c68
Kissamaagi <kissamaagi@github.com>
2021-08-16 13:34:17 +0300

    exercise 2.1 done

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+const Header = ({name}) => <h1>{name}</h1>
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part =>
+      <Part key={part.id} name={part.name} exercises={part.exercises} />)
+      }
+    </div>
+  )
+}
+
+const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+
+const Total = (props) => {
+
+  let total = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
+  return (
+    <p>Number of exercises: {total}</p>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      },
+      {
+        name: 'test tets test',
+        exercises: 5,
+        id: 5
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App;
